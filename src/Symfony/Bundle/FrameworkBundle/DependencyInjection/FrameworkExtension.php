496b27a9a8132d7f1c391595b7c47b31e665f0a8
Nicolas Rigaud <nicolas.rigaud@sensiolabs.com>
2024-01-25 17:29:16 +0100

    Move to a dedicated command in the Translation component

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
@@ -1435,6 +1435,7 @@ class FrameworkExtension extends Extension
             $container->removeDefinition('console.command.translation_extract');
             $container->removeDefinition('console.command.translation_pull');
             $container->removeDefinition('console.command.translation_push');
+            $container->removeDefinition('console.command.translation_xliff_update_sources');
 
             return;
         }
@@ -1510,6 +1511,12 @@ class FrameworkExtension extends Extension
 
         if ($container->hasDefinition('console.command.translation_extract')) {
             $container->getDefinition('console.command.translation_extract')->replaceArgument(6, $transPaths);
+        }
+
+        if ($container->hasDefinition('console.command.translation_xliff_update_sources')) {
+            $container->getDefinition('console.command.translation_xliff_update_sources')
+                ->replaceArgument(3, array_merge($transPaths, [$config['default_path']]))
+            ;
         }
 
         if (null === $defaultDir) {
