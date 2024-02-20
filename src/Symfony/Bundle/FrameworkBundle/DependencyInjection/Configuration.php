4222b745818bfa9079249d2c28e3bd91f7497c18
Pierre Rineau <pierre.rineau@makina-corpus.com>
2024-02-20 15:16:37 +0100

    [AccessToken] A component for fetching and caching remote services access tokens

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Configuration.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Configuration.php
@@ -156,6 +156,7 @@ class Configuration implements ConfigurationInterface
         $this->addRouterSection($rootNode);
         $this->addSessionSection($rootNode);
         $this->addRequestSection($rootNode);
+        $this->addAccessTokenSection($rootNode);
         $this->addAssetsSection($rootNode, $enableIfStandalone);
         $this->addAssetMapperSection($rootNode, $enableIfStandalone);
         $this->addTranslatorSection($rootNode, $enableIfStandalone);
@@ -714,6 +715,39 @@ class Configuration implements ConfigurationInterface
                                 ->end()
                                 ->beforeNormalization()->castToArray()->end()
                                 ->prototype('scalar')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+
+    private function addAccessTokenSection(ArrayNodeDefinition $rootNode): void
+    {
+        $rootNode
+            ->children()
+                ->arrayNode('access_token')
+                    ->info('access token manager configuration')
+                    ->canBeEnabled()
+                    ->children()
+                        ->arrayNode('lock')
+                            ->canBeDisabled()
+                            ->children()
+                                ->scalarNode('ttl')
+                                    ->info('The lock TTL value in seconds')
+                                    ->defaultValue(5)
+                                ->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('credentials')
+                            ->useAttributeAsKey('name')
+                            ->prototype('array')
+                                ->children()
+                                    ->scalarNode('url')
+                                        ->info('The URL of the credentials, scheme will determine which implementation to use')
+                                    ->end()
+                                ->end()
                             ->end()
                         ->end()
                     ->end()
