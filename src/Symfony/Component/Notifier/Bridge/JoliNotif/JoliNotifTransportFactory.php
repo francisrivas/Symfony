f8a2c79f434e921c334ffa8cba0ac2bfcdf7da6f
Ahmed Ghanem <ahmedghanem7361@gmail.com>
2024-07-09 12:24:06 +0300

    Remove `strict_types` declaration

--- src/Symfony/Component/Notifier/Bridge/JoliNotif/JoliNotifTransportFactory.php
+++ src/Symfony/Component/Notifier/Bridge/JoliNotif/JoliNotifTransportFactory.php
@@ -9,8 +9,6 @@
  * file that was distributed with this source code.
  */
 
-declare(strict_types=1);
-
 namespace Symfony\Component\Notifier\Bridge\JoliNotif;
 
 use Joli\JoliNotif\DefaultNotifier as JoliNotifier;
--- src/Symfony/Component/Notifier/Channel/DesktopChannel.php
+++ src/Symfony/Component/Notifier/Channel/DesktopChannel.php
@@ -9,8 +9,6 @@
  * file that was distributed with this source code.
  */
 
-declare(strict_types=1);
-
 namespace Symfony\Component\Notifier\Channel;
 
 use Symfony\Component\Messenger\Exception\ExceptionInterface;
