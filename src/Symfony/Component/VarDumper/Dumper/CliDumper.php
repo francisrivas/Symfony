af7abb5fbbc659798cd388cbccd491d6128f5c23
Artfaith <the.f8er@gmail.com>
2024-07-19 15:07:04 +0200

    Added support for FORCE_COLOR environment variable

--- src/Symfony/Component/VarDumper/Dumper/CliDumper.php
+++ src/Symfony/Component/VarDumper/Dumper/CliDumper.php
@@ -586,6 +586,11 @@ class CliDumper extends AbstractDumper
             return false;
         }
 
+        // Follow https://force-color.org/
+        if (!empty($_SERVER['FORCE_COLOR'])) {
+            return true;
+        }
+        
         // Follow https://no-color.org/
         if (isset($_SERVER['NO_COLOR']) || false !== getenv('NO_COLOR')) {
             return false;
